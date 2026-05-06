02c168fb3b5a8f7a0e956a7e8c8476725d3f2360
Arthur de Souza <arthurspedroso15@gmail.com>
2026-05-06 14:51:39 -0300

    fix

--- startup.sh
+++ startup.sh
@@ -5,10 +5,3 @@ apt-get update -y
 
 #Instala as dependencias
 apt-get install -y git docker.io docker-compose
-
-#Clona repositorio
-git clone https://github.com/arthurspedroso/demo-cloud-ufg.git /app
-
-#Sobe a aplicacao
-cd app
-docker-compose up -d
